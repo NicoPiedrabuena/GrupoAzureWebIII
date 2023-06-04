f77c13cae23e2217286709bed59b7a93db609336
stitchees <54647336+stitchees@users.noreply.github.com>
2023-06-04 20:57:11 -0300

    refactor home controller
    se agregaron 2 servicios que son referenciados en el controlador dependiendo la opcion elegida en el formulario

--- GrupoAzureWebIII/Controllers/HomeController.cs
+++ GrupoAzureWebIII/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Text.Json;
 using System.Text;
 using GrupoAzureWebIII.ViewModels;
+using GrupoAzureWebIII.Services;
 
 namespace GrupoAzureWebIII.Controllers
 {
@@ -13,30 +14,34 @@ namespace GrupoAzureWebIII.Controllers
         private readonly HttpClient _httpClient;
         private readonly AzureFunctionTuSecreto _AzureFunctionTuSecreto;
 
-      public HomeController(ILogger<HomeController> logger)
+        private readonly TwitterService _twitterService;
+        private readonly EmailService _emailService;
+        
+        private string enviarMailApiKey = "4o7pi4RkG5eeewDF7iVBIMYZw76k7YxBQadnMH_jA9FNAzFujT2_pQ==";
+        private string enviarTuitApiKey = "wnTR9oLhe31T1494bixlG0i_RTfrYwVJlYLj7NYLjKBfAzFuPGPOrg==";
+
+        private string enviarMailUrl = "https://connecttoapi.azurewebsites.net/api/SendEmail";
+        private string enviarTuitUrl = "https://connecttoapi.azurewebsites.net/api/TweetFunction";
+
+
+
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
             _httpClient = new HttpClient();
             _AzureFunctionTuSecreto = new AzureFunctionTuSecreto();
+            _twitterService = new TwitterService(enviarTuitUrl,enviarTuitApiKey);
+            _emailService = new EmailService(enviarMailUrl,enviarMailApiKey);
         }
 
         public IActionResult Index()
         {
             return View();
         }
-
-      
-        public async Task<IActionResult> GoAzure()
-        { 
-             await _AzureFunctionTuSecreto.CallAzureFunction("hola", "rey", 4);
-            return RedirectToAction("Confirmacion");
-        }
-
         public IActionResult Confirmacion()
         {
             return View();
         }
-
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
@@ -49,125 +54,40 @@ namespace GrupoAzureWebIII.Controllers
     [HttpPost]
     //editar elementos de entrada y a donde dirigirlos dependiendo la opcion ....
     public async Task<IActionResult> EnviarMensaje(FormViewModel form)
-
-
     {
-        string enviarMailApiKey = "4o7pi4RkG5eeewDF7iVBIMYZw76k7YxBQadnMH_jA9FNAzFujT2_pQ==";
-        string enviarTuitApiKey = "wnTR9oLhe31T1494bixlG0i_RTfrYwVJlYLj7NYLjKBfAzFuPGPOrg==";
-
-
+        
             if (form.publicarTwitter)
-        {
-            // Ejecutar lógica para publicar en Twitter utilizando la biblioteca adecuada
-            using (HttpClient client = new HttpClient())
             {
-                try
+                bool tweetPublicado = await _twitterService.PublicarTweet(form.mensaje, form.user);
+                if (tweetPublicado)
                 {
-                    // Crea el contenido del cuerpo (body) de la solicitud
-                    string requestBody = $"{{\"message\":\"{form.mensaje}\",\"name\":\"{form.user}\"}}";
-                        // Agrega los encabezados (headers) de la solicitud
-                        client.DefaultRequestHeaders.Add("x-functions-key", enviarTuitApiKey);
-                    // Realiza una solicitud POST a la API
-                    HttpResponseMessage response = await client.PostAsync("https://connecttoapi.azurewebsites.net/api/TweetFunction",
-                                                                    new StringContent(requestBody, Encoding.UTF8, "application/json"));
-                    // Verifica el código de estado de la respuesta
-                    if (response.IsSuccessStatusCode)
-                    {
-                        Console.WriteLine("Se realizo el tuit correctamente");
-                        // Lee el contenido de la respuesta como una cadena JSON
-                        string jsonResponse = await response.Content.ReadAsStringAsync();
-                        // Procesa la respuesta JSON según sea necesario
-                        Console.WriteLine(jsonResponse);
-                        return RedirectToAction("Confirmacion");
-                    }
-
-
-
-                    //HABRIA QUE GUARDAR EL TWIT SI SE MANDA DE MANERA CORRECTA
-
-
-
-                    else
-                    {
-                        // Si la respuesta no es exitosa, muestra el código de estado
-                        Console.WriteLine($"Error en la solicitud. Código de estado: {response.StatusCode}");
-                        
-                        ViewBag.mensajeError = "Algo salió mal";
-                        return View("Index");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    // Captura cualquier excepción ocurrida durante la solicitud
-                    Console.WriteLine($"Error: {ex.Message}");
-                }
-            }
-        }
-
-
-
-
-        else if (form.publicarEmail)
-        { // Preparar los datos para enviar en la solicitud HTTP
-
-
-
-            // Crea el contenido del cuerpo (body) de la solicitud
-            string requestBody = $"{{\"message\":\"{form.mensaje}\",\"user\":\"{form.user}\"}}";
-
-
-
-
-            // Crear una instancia de HttpClient
-            using (HttpClient client = new HttpClient())
-            {
-                // Agrega los encabezados (headers) de la solicitud
-                client.DefaultRequestHeaders.Add("x-functions-key", enviarMailApiKey);
-
-
-
-                // Hacer la solicitud HTTP al endpoint de la función "SendEmail"
-                HttpResponseMessage response = await client.PostAsync("https://connecttoapi.azurewebsites.net/api/SendEmail",
-                                                    new StringContent(requestBody, Encoding.UTF8, "application/json"));
-
-
-
-                // Verificar el estado de la respuesta HTTP
-                if (response.IsSuccessStatusCode)
-                {
-                    // Redirigir a una acción específica después de enviar el correo electrónico
+                    // Hacer algo después de publicar en Twitter
                     return RedirectToAction("Confirmacion");
-
-
-
-                    //HABRIA QUE GUARDAR EL MAIL SI SE MANDA DE MANERA CORRECTA
-
-
-
-
-
                 }
                 else
                 {
-                    // Si la respuesta no es exitosa, muestra el código de estado
-                    Console.WriteLine($"Error en la solicitud. Código de estado: {response.StatusCode}");
+                    ViewBag.mensajeError = "No se pudo publicar en Twitter";
+                    return View("Index");
                 }
-
-
-
             }
 
-
-
-
-        }
-
-
+            else if (form.publicarEmail)
+            {
+                bool mailEnviado = await _emailService.EnviarEmail(form.mensaje, form.user);
+                if (mailEnviado)
+                {
+                    // Hacer algo después de publicar en Twitter
+                    return RedirectToAction("Confirmacion");
+                }
+                else
+                {
+                    ViewBag.mensajeError = "No se pudo enviar el Mail";
+                    return View("Index");
+                }
+            }
 
         // Si no se seleccionó ninguna opción, puedes mostrar un mensaje de error o redirigir a una página de error
         return View("Error");
-
-
 
     }
    }
